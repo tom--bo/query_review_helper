d19cb680a9c2c884139b40ed09ec0b2e71289e02
tom--bo <otsuka.tt@gmail.com>
2021-05-04 14:23:02 +0900

    Get keys from MySQL

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Query Review Helper
 
-.
+- クエリとdb情報をなげるだけ.
+- table alias nameが合ってもoriginalのtable nameがとれる
+- クエリに含まれるテーブル一覧、そのindex一覧が取れる
--- mysql.go
+++ mysql.go
@@ -6,9 +6,10 @@ import (
 	"github.com/jmoiron/sqlx"
 	"log"
 	"strconv"
+	"strings"
 )
 
-func connectMySQL() {
+func connectMySQL() error{
 	var err error
 	mysqlHost := user + ":" + password + "@tcp(" + host + ":" + strconv.Itoa(port) + ")/" + database + "?loc=Local&parseTime=true"
 	if socket != "" {
@@ -18,39 +19,79 @@ func connectMySQL() {
 	db, err = sqlx.Open("mysql", mysqlHost)
 	if err != nil {
 		fmt.Println("Connection to MySQL fail.")
+		return err
 	}
+	return nil
 }
 
-
-type TableInfo struct {
+type IndexColumn struct {
 	IndexName string `db:"INDEX_NAME"`
 	Columns string `db:"columns"`
 }
 
-func getKeys(dbs, tbl string) {
+type IndexInfo struct {
+	pkColumns string
+	indexes map[string]string
+}
+
+func getKeys(dbs, tbl string) (IndexInfo, error){
+	indexInfo := IndexInfo{}
+	indexInfo.indexes = make(map[string]string)
 	q := `
 SELECT index_name, GROUP_CONCAT(column_name ORDER BY seq_in_index ASC) AS columns
-FROM information_schema.statistics WHERE (table_schema, table_name) = (:sname, :tname) GROUP BY index_name
+FROM information_schema.statistics WHERE (table_schema, table_name) = (:sname, :tname) GROUP BY index_name ORDER BY columns
 `
 	rows, err := db.NamedQuery(q, map[string]interface{}{"sname": dbs, "tname": tbl})
 	if err != nil {
 		log.Fatalf("db.Query(): %s\n", err)
+		return indexInfo, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		t := TableInfo{}
+		t := IndexColumn{}
 		err = rows.StructScan(&t)
 		if err != nil {
 			fmt.Println(err.Error())
-			continue
+			return indexInfo, err
 		}
-
-		fmt.Printf("%v\n", t)
+		if t.IndexName == "PRIMARY" {
+			indexInfo.pkColumns = t.Columns
+		} else {
+			indexInfo.indexes[t.Columns] = t.IndexName
+		}
 	}
 	if err = rows.Err(); err != nil {
 		log.Fatalf("rows.Err(): %s\n", err)
+		return indexInfo, err
 	}
+	return indexInfo, nil
+}
+
+type TableCardinality struct {
+	Cardinality int `db:"cardinality"`
+}
+
+func samplingColumnCardinality(table, pkColumn, column string, limit int) (int, error) {
+	pkASC := strings.ReplaceAll(pkColumn, ",", " ASC, ") + " ASC"
+	pkDESC := strings.ReplaceAll(pkColumn, ",", " DESC, ") + " DESC"
+
+	q := fmt.Sprintf(`
+SELECT COUNT(DISTINCT %s) as cardinality
+FROM (
+  (SELECT %s FROM %s ORDER BY %s LIMIT %d)
+  UNION DISTINCT
+  (SELECT %s FROM %s ORDER BY %s LIMIT %d)
+) as tmp
+`, column, column, table, pkASC, limit, column, table, pkDESC, limit)
+
+	c := TableCardinality{}
+	err := db.Get(&c, q)
+	if err != nil {
+		return 0, err
+	}
+
+	return c.Cardinality, nil
 }
 
 
